f48d39d0b3ecf9da0e3e09a56b4cd53c82e61045
Jatin Singh <70967350+beatenstratblues@users.noreply.github.com>
2025-03-14 16:25:44 +0530

    words page added, in progress

--- src/pages/WordsPages.jsx
+++ src/pages/WordsPages.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import AudioRecorder from '../components/AudioRecorder'
 
 const WordsPages = () => {
   return (
-    <div>WordsPages</div>
+    <div className='wordPage'>
+      <div className='pageTitle'>Type in any word and test your accent!!<img width="40" height="40" src="https://img.icons8.com/fluency/48/dictionary.png" alt="dictionary"/></div>
+      <AudioRecorder/>
+    </div>
   )
 }
 
